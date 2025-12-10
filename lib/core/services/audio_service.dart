d4ddf9a924c4e60eef341d62a37c16c25d16222a
Pramod Pal <pramod.pal@neo-world.com>
2025-12-10 16:48:53 +0530

    feat: Add vibration feedback when playing and stopping the ringtone.

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:vibration/vibration.dart';
 
 class AudioService {
   final AudioPlayer _player = AudioPlayer();
@@ -7,9 +8,14 @@ class AudioService {
     // Release mode needs asset source prefix
     await _player.stop(); // Stop any previous sound
     await _player.play(AssetSource('audio/ringtone.mp3'));
+
+    if (await Vibration.hasVibrator()) {
+      Vibration.vibrate(duration: 5000);
+    }
   }
 
   Future<void> stop() async {
     await _player.stop();
+    Vibration.cancel();
   }
 }
